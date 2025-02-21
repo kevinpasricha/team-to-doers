f6e4ac6b6eb2c7b9cda907b2c951d3436648dc4c
MatthewPelter <pelter.matthew@gmail.com>
2025-02-20 20:07:21 -0500

    Updated Frontend imports

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import API from "../src/api";
+import API from "../api";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
